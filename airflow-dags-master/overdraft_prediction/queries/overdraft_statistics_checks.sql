7cc52e66835b1538ef196a9d5d0412b5e46f63a6
maliadskaia <97615010+maliadskaia@users.noreply.github.com>
2022-01-24 09:30:20 +0200

    Update overdraft_statistics_checks.sql

--- airflow-dags-master/overdraft_prediction/queries/overdraft_statistics_checks.sql
+++ airflow-dags-master/overdraft_prediction/queries/overdraft_statistics_checks.sql
@@ -12,20 +12,14 @@ with combined_table
     AS --Part 1: combined table is the part 1. which create in a customer level the all the files data between the two diffrent files:
     -- the production and the new prediction
     (
-        SELECT CASE
-                   WHEN n.bank_account_id IS NULL THEN c.bank_account_number
-                   ELSE n.bank_account_id
-                   END           AS bank_account_number,
+        SELECT COALESCE(n.bank_account_id, c.bank_account_number)    AS bank_account_number,
                c.overdraft_limit,
                c.expiration_date,
-               n.expiration_date AS new_expiration,
+               n.expiration_date                                     AS new_expiration,
                n.reason,
-               n.prediction      AS new_limit,
-               n.DAG_RUN         as CREATED_AT,
-               CASE
-                   WHEN c.bank_account_number IS NULL THEN 'new'
-                   ELSE 'existing'
-                   END           AS model_type
+               n.prediction                                          AS new_limit,
+               n.DAG_RUN                                             as CREATED_AT,
+               IFF(c.bank_account_number IS NULL, 'new', 'existing') AS model_type
         FROM "DWH"."ML_OVERDRAFT_POST_PREDICTION" n
                  FULL OUTER JOIN "LILI_ANALYTICS"."ODS"."OVERDRAFT_LIMITS" c
                                  ON n.bank_account_id = c.bank_account_number
@@ -34,12 +28,8 @@ with combined_table
     AS -- overdraft_logic is the part 2. which adds additonal data in the customer level that are not in the files+create logics about the customers
     ( -- logic such as - who we can change and who dont
         SELECT ct.*,
-               CASE
-                   WHEN (date(EXPIRATION_DATE) not in (date(CURRENT_DATE()),
-                                                       date(dateadd(DAY, 1, current_date())))
-                       AND OVERDRAFT_LIMIT > 0) THEN 'unchangeable'
-                   ELSE 'changeable'
-                   END AS status,
+               IFF(date(EXPIRATION_DATE) not in (DATE(%(dag_run_start_date)s), DATE(%(dag_run_start_date)s) + 1)
+                       AND OVERDRAFT_LIMIT > 0, 'unchangeable', 'changeable') AS status,
                dw.BANK_ACCOUNT_ID,
                dw.pro_customer,
                dw.ACCOUNT_LEGIT,
@@ -52,13 +42,12 @@ with combined_table
                                  BANK_ACCOUNT_NUMBER,
                                  PRO_CUSTOMER
                  FROM "LILI_ANALYTICS"."ODS"."MYSQL_DW_CUSTOMER_MONTHLY_NEW"
-                 where year = year(current_date)
-                   and month = month(current_date)
+                 where year = year(DATE(%(dag_run_start_date)s))
+                   and month = month(DATE(%(dag_run_start_date)s))
              ) dw
              ON ct.BANK_ACCOUNT_NUMBER = dw.BANK_ACCOUNT_NUMBER
-                 INNER JOIN
-                 (SELECT * FROM "LILI_ANALYTICS"."ODS"."MYSQL_ACCOUNT_CURRENT_BALANCE") b
-                 ON dw.BANK_ACCOUNT_ID = b.BANK_ACCOUNT_ID
+                 INNER JOIN "LILI_ANALYTICS"."ODS"."MYSQL_ACCOUNT_CURRENT_BALANCE" b
+                            ON dw.BANK_ACCOUNT_ID = b.BANK_ACCOUNT_ID
     )
    -- SELECT * FROM overdraft_logic
 -- );
@@ -153,7 +142,7 @@ with combined_table
                    AS revoked_new_prediction,
 
                count(DISTINCT CASE
-                                  WHEN (OVERDRAFT_LIMIT > 0 AND NEW_LIMIT = 0 AND NEW_EXPIRATION < CURRENT_DATE() + 1)
+                                  WHEN (OVERDRAFT_LIMIT > 0 AND NEW_LIMIT = 0 AND NEW_EXPIRATION < DATE(%(dag_run_start_date)s) + 1)
                                       THEN BANK_ACCOUNT_NUMBER
                                   ELSE NULL END)
                    AS revoked_no_expiration_new_prediction,
