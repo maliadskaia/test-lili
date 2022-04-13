4b035715950cef308d61dd58e88fa711ed0976e1
maliadskaia <amaliadskaia@opsguru.io>
2022-04-13 09:50:45 +0200

    v

--- test_01.py
+++ test_01.py
@@ -23,7 +23,7 @@ import os, boto3
 
 
 s3_resource = boto3.resource('s3',
-  aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+  aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
   aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
   region_name=os.environ["AWS_REGION"]
   )
